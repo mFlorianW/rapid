e3d6f053efec01e366da5dd4562a7b8afebfeee9
Florian Weßel <florianwessel@gmx.net>
2024-12-17 16:35:29 +0100

    storage: Use test fixture for easier database setup

--- tests/storage/test_SqliteSessionDatabase.cpp
+++ tests/storage/test_SqliteSessionDatabase.cpp
@@ -33,60 +33,78 @@ class SqliteSessionDatabaseEventListener : public SqliteDatabaseTestEventlistene
     }
 };
 
+struct TestFixture
+{
+    SessionData session1 = Sessions::getTestSession3();
+    SessionData session2 = Sessions::getTestSession4();
+
+    void setupTestDatabase(SqliteSessionDatabase& db)
+    {
+        storeSession1(db);
+        storeSession2(db);
+    }
+
+    void storeSession1(SqliteSessionDatabase& db)
+    {
+        auto const session1 = Sessions::getTestSession3();
+        auto storeResult = db.storeSession(session1);
+        storeResult->waitForFinished();
+        REQUIRE(storeResult->getResult() == Result::Ok);
+    }
+
+    void storeSession2(SqliteSessionDatabase& db)
+    {
+        auto const session2 = Sessions::getTestSession4();
+        auto storeResult = db.storeSession(session2);
+        storeResult->waitForFinished();
+        REQUIRE(storeResult->getResult() == Result::Ok);
+    }
+};
+
 } // namespace
 
 CATCH_REGISTER_LISTENER(SqliteSessionDatabaseEventListener)
 
-TEST_CASE("The SqliteSessionDatabase shall store as session and shall emit the session added signal.")
+TEST_CASE_METHOD(TestFixture,
+                 "The SqliteSessionDatabase shall store as session and shall emit the session added signal.")
 {
+    auto sessionAddedSignalEmitted = false;
+    auto addedIndex = std::size_t{123456};
 
     SECTION("Store session and emit signal")
     {
-
         auto db = SqliteSessionDatabase{getTestDatabaseFile()};
-        auto sessionAddedSignalEmitted = false;
-        auto addedIndex = std::size_t{123456};
         std::ignore = db.sessionAdded.connect([&sessionAddedSignalEmitted, &addedIndex](std::size_t index) {
             sessionAddedSignalEmitted = true;
             addedIndex = index;
         });
-
-        auto insertResult = db.storeSession(Sessions::getTestSession3());
-        insertResult->waitForFinished();
-        REQUIRE(insertResult->getResult() == Result::Ok);
+        storeSession1(db);
         REQUIRE(addedIndex == 0);
         REQUIRE(sessionAddedSignalEmitted == true);
 
         sessionAddedSignalEmitted = false;
-        insertResult = db.storeSession(Sessions::getTestSession4());
-        insertResult->waitForFinished();
-        REQUIRE(insertResult->getResult() == Result::Ok);
+        storeSession2(db);
         REQUIRE(addedIndex == 1);
         REQUIRE(sessionAddedSignalEmitted == true);
     }
 
     SECTION("Store session and all instances shall emit the signal")
     {
-
         auto db = SqliteSessionDatabase{getTestDatabaseFile()};
         auto db2 = SqliteSessionDatabase{getTestDatabaseFile()};
-        auto sessionAddedSignalEmitted = false;
-        auto addedIndex = std::size_t{123456};
         std::ignore = db2.sessionAdded.connect([&sessionAddedSignalEmitted, &addedIndex](std::size_t index) {
             sessionAddedSignalEmitted = true;
             addedIndex = index;
         });
 
-        auto insertResult = db.storeSession(Sessions::getTestSession3());
-        REQUIRE_COMPARE_WITH_TIMEOUT(insertResult->getResult(), Result::Ok, 1s);
-        insertResult->waitForFinished();
-        REQUIRE(insertResult->getResult() == Result::Ok);
+        storeSession1(db);
         REQUIRE(addedIndex == 0);
         REQUIRE(sessionAddedSignalEmitted == true);
     }
 }
 
-TEST_CASE("The SqliteSessionDatabase shall store a session and the session shall be directly read again.")
+TEST_CASE_METHOD(TestFixture,
+                 "The SqliteSessionDatabase shall store a session and the session shall be directly read again.")
 {
     auto db = SqliteSessionDatabase{getTestDatabaseFile()};
     auto addedIndex = std::size_t{123456};
@@ -94,15 +112,13 @@ TEST_CASE("The SqliteSessionDatabase shall store a session and the session shall
         addedIndex = index;
     });
 
-    auto const insertResult = db.storeSession(Sessions::getTestSession3());
-    insertResult->waitForFinished();
-    REQUIRE(insertResult->getResult() == Result::Ok);
+    storeSession1(db);
     REQUIRE(addedIndex == 0);
 
     auto const readResult = db.getSessionByIndex(addedIndex);
     REQUIRE(readResult.has_value() == true);
     // NOLINTBEGIN(bugprone-unchecked-optional-access)
-    REQUIRE(readResult.value() == Sessions::getTestSession3());
+    REQUIRE(readResult.value() == session1);
 
     auto const asyncReadResult = db.getSessionByIndexAsync(addedIndex);
     REQUIRE_COMPARE_WITH_TIMEOUT(asyncReadResult->getResult(), Result::Ok, std::chrono::milliseconds{10});
@@ -112,9 +128,13 @@ TEST_CASE("The SqliteSessionDatabase shall store a session and the session shall
     // NOLINTEND(bugprone-unchecked-optional-access)
 }
 
-TEST_CASE("The SqliteSessionDatabase shall store a already stored session under the same index and shall emit session "
-          "updated.")
+TEST_CASE_METHOD(
+    TestFixture,
+    "The SqliteSessionDatabase shall store a already stored session under the same index and shall emit session "
+    "updated.")
 {
+    auto lapData = Rapid::Common::LapData{};
+    lapData.addSectorTimes({{"00:23:32.003"}, {"00:23:32.004"}, {"00:23:32.005"}});
     SECTION("Update session and emit the signal")
     {
         auto db = SqliteSessionDatabase{getTestDatabaseFile()};
@@ -123,21 +143,13 @@ TEST_CASE("The SqliteSessionDatabase shall store a already stored session under 
             updatedIndex = index;
         });
 
-        auto insertResult = db.storeSession(Sessions::getTestSession3());
-        insertResult->waitForFinished();
-        REQUIRE(insertResult->getResult() == Result::Ok);
-        insertResult = db.storeSession(Sessions::getTestSession4());
-        insertResult->waitForFinished();
-        REQUIRE(insertResult->getResult() == Result::Ok);
-
-        auto lapData = Rapid::Common::LapData{};
-        lapData.addSectorTimes({{"00:23:32.003"}, {"00:23:32.004"}, {"00:23:32.005"}});
+        setupTestDatabase(db);
         auto session1 = Sessions::getTestSession3();
         session1.addLap(lapData);
         auto session2 = Sessions::getTestSession4();
         session2.addLap(lapData);
 
-        insertResult = db.storeSession(session1);
+        auto insertResult = db.storeSession(session1);
         insertResult->waitForFinished();
         REQUIRE(insertResult->getResult() == Result::Ok);
         REQUIRE(updatedIndex == 0);
@@ -159,17 +171,9 @@ TEST_CASE("The SqliteSessionDatabase shall store a already stored session under 
             updatedIndex = index;
         });
 
-        auto insertResult = db.storeSession(Sessions::getTestSession3());
-        REQUIRE_COMPARE_WITH_TIMEOUT(insertResult->getResult(), Result::Ok, 1s);
-        insertResult = db.storeSession(Sessions::getTestSession4());
-        REQUIRE_COMPARE_WITH_TIMEOUT(insertResult->getResult(), Result::Ok, 1s);
-
-        auto lapData = Rapid::Common::LapData{};
-        lapData.addSectorTimes({{"00:23:32.003"}, {"00:23:32.004"}, {"00:23:32.005"}});
-        auto session1 = Sessions::getTestSession3();
         session1.addLap(lapData);
 
-        insertResult = db.storeSession(session1);
+        auto insertResult = db.storeSession(session1);
         REQUIRE_COMPARE_WITH_TIMEOUT(insertResult->getResult(), Result::Ok, 1s);
         CHECK(insertResult->getResult() == Result::Ok);
         CHECK(updatedIndex == 0);
@@ -177,66 +181,52 @@ TEST_CASE("The SqliteSessionDatabase shall store a already stored session under 
     }
 }
 
-TEST_CASE("The SqliteSessionDatabase shall gives the number of stored sessions and should return the correct session"
-          "for that index.")
+TEST_CASE_METHOD(
+    TestFixture,
+    "The SqliteSessionDatabase shall gives the number of stored sessions and should return the correct session"
+    "for that index.")
 {
     auto db = SqliteSessionDatabase{getTestDatabaseFile()};
-    auto const session1 = Sessions::getTestSession3();
-    auto const session2 = Sessions::getTestSession4();
-    constexpr auto expectedSessionCount = 2;
+    setupTestDatabase(db);
+    SECTION("Correct session count is returned")
+    {
+        constexpr auto expectedSessionCount = 2;
+        auto const sessionCount = db.getSessionCount();
+        REQUIRE(sessionCount == expectedSessionCount);
+    }
 
-    // Prepare database.
-    auto storeResult = db.storeSession(session1);
-    storeResult->waitForFinished();
-    REQUIRE(storeResult->getResult() == Result::Ok);
-    storeResult = db.storeSession(session2);
-    storeResult->waitForFinished();
-    REQUIRE(storeResult->getResult() == Result::Ok);
-
-    auto const sessionCount = db.getSessionCount();
-    REQUIRE(sessionCount == expectedSessionCount);
-
-    REQUIRE(db.getSessionByIndex(0).value_or(SessionData{}) == session1);
-    REQUIRE(db.getSessionByIndex(1).value_or(SessionData{}) == session2);
-    REQUIRE(db.getSessionByIndex(2) == std::nullopt);
+    SECTION("Correct session data is returned")
+    {
+        REQUIRE(db.getSessionByIndex(0).value_or(SessionData{}) == session1);
+        REQUIRE(db.getSessionByIndex(1).value_or(SessionData{}) == session2);
+        REQUIRE(db.getSessionByIndex(2) == std::nullopt);
+    }
 }
 
-TEST_CASE("The SqliteSessionDatabase shall delete a session under the index and shall emit the sessionDelete signal "
-          "with the index.")
+TEST_CASE_METHOD(
+    TestFixture,
+    "The SqliteSessionDatabase shall delete a session under the index and shall emit the sessionDelete signal "
+    "with the index.")
 {
     auto db = SqliteSessionDatabase{getTestDatabaseFile()};
-    auto const session1 = Sessions::getTestSession3();
-    auto const session2 = Sessions::getTestSession4();
+    setupTestDatabase(db);
     auto deletedIndex = std::size_t{123456};
     constexpr auto indexToDelete = 1;
     std::ignore = db.sessionDeleted.connect([&deletedIndex](std::size_t index) {
         deletedIndex = index;
     });
 
-    // Prepare database.
-    auto storeResult = db.storeSession(session1);
-    storeResult->waitForFinished();
-    REQUIRE(storeResult->getResult() == Result::Ok);
-    storeResult = db.storeSession(session2);
-    storeResult->waitForFinished();
-    REQUIRE(storeResult->getResult() == Result::Ok);
-    REQUIRE(db.getSessionCount() == 2);
-
     db.deleteSession(indexToDelete);
     REQUIRE(deletedIndex == indexToDelete);
     REQUIRE(db.getSessionCount() == 1);
 }
 
-TEST_CASE("The SqlieSessionDatabase shall emit session deteled on referential integrity changes")
+TEST_CASE_METHOD(TestFixture, "The SqlieSessionDatabase shall emit session deteled on referential integrity changes")
 {
     auto db = SqliteSessionDatabase{getTestDatabaseFile()};
-    auto const session1 = Sessions::getTestSession3();
+    storeSession1(db);
     auto deletedIndex = std::size_t{123456};
     constexpr auto indexToDelete = 0;
-
-    auto storeResult = db.storeSession(session1);
-    storeResult->waitForFinished();
-    CHECK(storeResult->getResult() == Result::Ok);
     std::ignore = db.sessionDeleted.connect([&deletedIndex](std::size_t index) {
         deletedIndex = index;
     });
@@ -248,27 +238,21 @@ TEST_CASE("The SqlieSessionDatabase shall emit session deteled on referential in
           SQLITE_OK);
     REQUIRE(deletedIndex == indexToDelete);
 }
-TEST_CASE("The SqliteSessionDatabase shall give the session meta data for a index.")
+
+TEST_CASE_METHOD(TestFixture, "The SqliteSessionDatabase shall give the session meta data for a index.")
 {
     auto db = SqliteSessionDatabase{getTestDatabaseFile()};
-
-    auto const session0 = Sessions::getTestSession3();
-    auto storeResult = db.storeSession(Sessions::getTestSession3());
-    REQUIRE_COMPARE_WITH_TIMEOUT(storeResult->getResult(), Rapid::System::Result::Ok, std::chrono::seconds{1});
-
-    auto const session1 = Sessions::getTestSession4();
-    storeResult = db.storeSession(session1);
-    REQUIRE_COMPARE_WITH_TIMEOUT(storeResult->getResult(), Rapid::System::Result::Ok, std::chrono::seconds{1});
+    setupTestDatabase(db);
 
     auto loadResult = db.getSessionMetaDataByIndexAsync(0);
-    REQUIRE_COMPARE_WITH_TIMEOUT(loadResult->getResult(), Rapid::System::Result::Ok, std::chrono::seconds{1});
-    CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getSessionDate() == session0.getSessionDate());
-    CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getSessionTime() == session0.getSessionTime());
-    CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getTrack() == session0.getTrack());
-
-    loadResult = db.getSessionMetaDataByIndexAsync(1);
     REQUIRE_COMPARE_WITH_TIMEOUT(loadResult->getResult(), Rapid::System::Result::Ok, std::chrono::seconds{1});
     CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getSessionDate() == session1.getSessionDate());
     CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getSessionTime() == session1.getSessionTime());
     CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getTrack() == session1.getTrack());
+
+    loadResult = db.getSessionMetaDataByIndexAsync(1);
+    REQUIRE_COMPARE_WITH_TIMEOUT(loadResult->getResult(), Rapid::System::Result::Ok, std::chrono::seconds{1});
+    CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getSessionDate() == session2.getSessionDate());
+    CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getSessionTime() == session2.getSessionTime());
+    CHECK(loadResult->getResultValue().value_or(SessionMetaData{}).getTrack() == session2.getTrack());
 }
