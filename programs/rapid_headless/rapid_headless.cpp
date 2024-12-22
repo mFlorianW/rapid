601c22ed3d9403a26bcf4fa22897b269164ba4f4
Florian Weßel <florianwessel@gmx.net>
2024-12-22 14:57:10 +0100

    rapid_headless: Add program options to enable GPS fake source

--- programs/rapid_headless/rapid_headless.cpp
+++ programs/rapid_headless/rapid_headless.cpp
@@ -10,9 +10,11 @@
 #include <SqliteSessionDatabase.hpp>
 #include <SqliteTrackDatabase.hpp>
 #include <array>
+#include <boost/program_options.hpp>
 #include <csignal>
 #include <fstream>
 #include <pwd.h>
+#include <spdlog/spdlog.h>
 #include <sstream>
 #include <string>
 #include <unistd.h>
@@ -22,6 +24,7 @@ using namespace Rapid::System;
 using namespace Rapid::Positioning;
 using namespace Rapid::Storage;
 using namespace Rapid::LappyHeadless;
+using namespace boost::program_options;
 
 namespace
 {
@@ -53,6 +56,11 @@ std::vector<Rapid::Common::PositionData> loadCsvPositions(std::string const file
     return positions;
 }
 
+void printHelp(options_description const& opts)
+{
+    std::cout << opts << "\n";
+}
+
 } // namespace
 
 int main(int argc, char** argv)
@@ -66,8 +74,46 @@ int main(int argc, char** argv)
     sigaction(SIGINT, &action, nullptr);
     sigaction(SIGTERM, &action, nullptr);
 
+    auto options = options_description{"Options"};
+    std::string gpsSourceFile{};
+    // clang-format off
+    options.add_options()
+        ("help,h", "Show options overivew")
+        ("gps-fake,g", "Enables a fake GPS source (useful for testing)")
+        ("gps-source-file,f", value<std::string>(&gpsSourceFile), "Path to CSV file that contains GPS positions (useful for testing)")
+    ;
+    // clang-format on
+    variables_map optionsMap;
+    try {
+        store(parse_command_line(argc, argv, options), optionsMap);
+    } catch (std::exception const& e) {
+        SPDLOG_ERROR("Inavlid option: {}", e.what());
+        printHelp(options);
+    }
+
+    if (optionsMap.count("help") > 0) {
+        printHelp(options);
+        return 0;
+    }
+
+    if (optionsMap.contains("gps-source-file") > 0) {
+        gpsSourceFile = optionsMap["gps-source-file"].as<std::string>();
+    }
+
+    if (optionsMap.count("gps-fake") > 0 and gpsSourceFile.empty()) {
+        SPDLOG_ERROR("Fake GPS enabled but no GPS position source file passed.");
+        printHelp(options);
+        return 0;
+    }
+
+    if (gpsSourceFile.empty()) {
+        SPDLOG_ERROR("No GPS position source file passed.");
+        printHelp(options);
+        return 0;
+    }
+
     // Load GPS position file
-    auto positions = loadCsvPositions("/home/florian/Coding/rapid/laps/Oschersleben.csv");
+    auto positions = loadCsvPositions(gpsSourceFile);
     auto positionProvider = ConstantGpsPositionProvider{positions};
     positionProvider.setVelocityInMeterPerSecond(80.3333);
     positionProvider.start();
