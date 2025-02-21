59cf1c5855c930b7251d16642a5cfead8b9c8f38
Florian Weßel <florianwessel@gmx.net>
2025-02-21 20:09:07 +0100

    rest: Make the QRestClient usable in QML

--- libs/rapid/rest/qt/QRestClient.hpp
+++ libs/rapid/rest/qt/QRestClient.hpp
@@ -5,24 +5,45 @@
 #pragma once
 
 #include "rest/IRestClient.hpp"
+#include <QObject>
 #include <QtGlobal>
 
 namespace Rapid::Rest
 {
-
 struct QRestClientPrivate;
+
+/**
+ * @copydoc Rapid::Rest::IRestClient
+ */
 class QRestClient : public IRestClient
 {
-    Q_DISABLE_COPY_MOVE(QRestClient)
+    Q_GADGET
 public:
+    Q_DISABLE_COPY_MOVE(QRestClient)
+
+    /**
+     * Creates an instance of the @ref QRestClient
+     */
     QRestClient();
 
+    /**
+     * Default destructor
+     */
     ~QRestClient() noexcept override;
 
+    /**
+     * @copydoc Rapid::Rest::IRestClient::setServerAddress
+     */
     void setServerAddress(std::string const& url) noexcept override;
 
+    /**
+     * @copydoc Rapid::Rest::IRestClient::setServerPort
+     */
     void setServerPort(std::uint16_t port) noexcept override;
 
+    /**
+     * @copydoc Rapid::Rest::IRestClient::execute
+     */
     std::shared_ptr<RestCall> execute(RestRequest const& request) noexcept override;
 
 private:
