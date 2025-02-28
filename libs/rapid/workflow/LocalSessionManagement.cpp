f59578f3487c6e6c76f3b07178a6e956f1b1c382
Florian Weßel <florianwessel@gmx.net>
2025-02-28 22:42:15 +0100

    workflow: Introduce LocalSessionManagement for none Qt applications

---
+++ libs/rapid/workflow/LocalSessionManagement.cpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2025 All contributors
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "LocalSessionManagement.hpp"
+
+namespace Rapid::Workflow
+{
+
+LocalSessionManagement::LocalSessionManagement(Storage::ISessionDatabase* db)
+    : mDb{db}
+{
+    assert(mDb != nullptr);
+}
+
+std::shared_ptr<Storage::GetSessionResult> LocalSessionManagement::getSession(Common::SessionMetaData const& metadata)
+{
+    return mDb->getSessionByMetadataAsync(metadata);
+}
+
+} // namespace Rapid::Workflow
