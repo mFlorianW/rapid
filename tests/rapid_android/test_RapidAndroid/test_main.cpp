9f5d4224af8f7843cfbe00d0fdd67d8454c4dd6b
Florian Weßel <florianwessel@gmx.net>
2025-03-19 21:46:10 +0100

    rapid_android: Introduce ContextMenuModel

---
+++ tests/rapid_android/test_RapidAndroid/test_main.cpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 - 2025 All contributors
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <catch2/catch_session.hpp>
+#include <system/qt/RapidQApplication.hpp>
+#include <testhelper/qt/QtTestHelper.hpp>
+
+QT_CATCH2_TEST_MAIN();
